7d6f1a2ec984941c242e32437d88621c5ae5081c
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-12-06 11:58:56 +0500

    Update lollyPage.js

--- src/template/lollyPage.js
+++ src/template/lollyPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
-import { Lolly } from '../components/Lolly'
+import Lolly from '../components/Lolly'
+
 import Result from '../components/Result'
 
 const lollyPage = ({ pageContext: {
